1e2c69c1aef906e148855c2ce055329b92862b23
Roy Allen Nidoy <allen@gappify.com>
2025-01-22 00:22:23 +0800

    build

--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Typography } from "@material-ui/core";
-const Footer = () => (
-  <Typography color="textSecondary" align="center" variant="body2">
-    Group 07 | Cui, Domingo, Nidoy, Pineda, Sacay
-  </Typography>
-);
-export default Footer;
